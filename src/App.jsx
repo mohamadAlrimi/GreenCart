7a2ed9395e43aefd800ebd620fb668a38be38566
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2025-04-15 00:48:30 +0300

    I Finished the Add Product section

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ import AddAddress from "./pages/AddAddress";
 import MyOrders from "./pages/MyOrders";
 import SellerLogin from "./components/seller/SellerLogin";
 import SellerLayout from "./pages/seller/SellerLayout";
+import AddProduct from "./pages/seller/AddProduct";
+import Orders from "./pages/seller/Orders";
+import ProductList from "./pages/seller/ProductList";
 
 const App = () => {
   const isSellerPath =useLocation().pathname.includes("seller")
@@ -33,7 +36,9 @@ const App = () => {
         <Route path="/add-address" element={<AddAddress/>}/>
         <Route path="/my-orders" element={<MyOrders/>}/>
         <Route path="/seller" element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
-
+            <Route index element={isSeller ? <AddProduct/> : null}/>
+            <Route path="product-list" element={<ProductList/>}/>
+            <Route path="orders" element={<Orders/>}/>
         </Route>
         </Routes>
       </div>
